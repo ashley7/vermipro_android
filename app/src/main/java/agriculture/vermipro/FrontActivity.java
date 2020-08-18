d998b62f6676796a5d8b3f4f26118669b4cf3322
THEMBO CHARLES LWANGA <ashley7520charles@gmail.com>
2020-08-18 13:56:35 +0300

    Clean ups

--- app/src/main/java/agriculture/vermipro/FrontActivity.java
+++ app/src/main/java/agriculture/vermipro/FrontActivity.java
@@ -40,11 +40,10 @@ public class FrontActivity extends AppCompatActivity {
 
     private TextView myorders;
     private ArrayList<VermiproHelper> vermiproHelperArrayListroHelper;
-    private GridView features_products,categories;
-
+    private GridView categories;
     private PostResponseAsyncTask postResponseAsyncTask;
     private FunDapter<VermiproHelper> fundupter;
-    private VermiproHelper category;
+
     private BindDictionary<VermiproHelper> dict;
 
     @Override
@@ -56,7 +55,7 @@ public class FrontActivity extends AppCompatActivity {
 
         myorders = findViewById(R.id.myorders);
         categories = findViewById(R.id.categories);
-        features_products = findViewById(R.id.features_products);
+
 
         myorders.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -66,14 +65,11 @@ public class FrontActivity extends AppCompatActivity {
         });
 
 
-
-
         postResponseAsyncTask = new PostResponseAsyncTask(FrontActivity.this, true, new AsyncResponse() {
             @Override
             public void processFinish(String s) {
 
                 try {
-
                     vermiproHelperArrayListroHelper = new JsonConverter<VermiproHelper>().toArrayList(s, VermiproHelper.class);
 
                     dict = new BindDictionary<>();
@@ -85,8 +81,25 @@ public class FrontActivity extends AppCompatActivity {
                         }
                     });
 
+                    dict.addDynamicImageField(R.id.image, new StringExtractor<VermiproHelper>() {
+                        @Override
+                        public String getStringValue(VermiproHelper item, int position) {
+                            return item.image;
+
+                        }
+                    }, new DynamicImageLoader() {
+                        @Override
+                        public void loadImage(String url, ImageView view) {
+                            Picasso.get().load(IMAGE_URL+"/product_images/" + url)
+                                    .placeholder(R.drawable.placeholder)
+                                    .resize(180,180)
+                                    .error(R.drawable.placeholder).into(view);
+                        }
+                    });
+
+
                     fundupter = new FunDapter<>(FrontActivity
-                            .this, vermiproHelperArrayListroHelper, R.layout.front_products_layout, dict);
+                            .this, vermiproHelperArrayListroHelper, R.layout.category_layout, dict);
 
                     categories.setAdapter(fundupter);
 
@@ -94,7 +107,8 @@ public class FrontActivity extends AppCompatActivity {
                         @Override
                         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
-                            category = vermiproHelperArrayListroHelper.get(position);
+                            VermiproHelper category  = vermiproHelperArrayListroHelper.get(position);
+
                             Intent intent = new Intent(FrontActivity.this, ProductsActivity.class);
                             intent.putExtra("category_id", ""+category.cat_id);
                             intent.putExtra("category_name", ""+category.cat_name);
@@ -110,111 +124,6 @@ public class FrontActivity extends AppCompatActivity {
         });
         postResponseAsyncTask.execute(URL+"categories");
 
-
-        postResponseAsyncTask = new PostResponseAsyncTask(FrontActivity.this, false,
-                new AsyncResponse() {
-                    @Override
-                    public void processFinish(String s) {
-
-                        Log.d("DATA",s);
-
-                        try {
-
-                            vermiproHelperArrayListroHelper =  new JsonConverter<VermiproHelper>().toArrayList(s, VermiproHelper.class);
-
-                            dict = new BindDictionary<>();
-
-                            dict.addStringField(R.id.name, new StringExtractor<VermiproHelper>() {
-                                @Override
-                                public String getStringValue(VermiproHelper item, int position) {
-                                    return ""+item.name;
-                                }
-                            });
-
-                            dict.addStringField(R.id.price, new StringExtractor<VermiproHelper>() {
-                                @Override
-                                public String getStringValue(VermiproHelper item, int position) {
-                                    return "Ush "+item.price;
-                                }
-                            });
-
-                            dict.addDynamicImageField(R.id.image, new StringExtractor<VermiproHelper>() {
-                                @Override
-                                public String getStringValue(VermiproHelper item, int position) {
-                                    return item.image;
-                                }
-                            }, new DynamicImageLoader() {
-                                @Override
-                                public void loadImage(String url, ImageView view) {
-                                    Picasso.get().load(IMAGE_URL+"/product_images/" + url)
-                                            .placeholder(R.drawable.placeholder)
-                                            .resize(180,180)
-                                            .error(R.drawable.placeholder).into(view);
-                                }
-                            });
-
-                            fundupter = new FunDapter<>(FrontActivity
-                                    .this, vermiproHelperArrayListroHelper, R.layout.front_products_layout, dict);
-
-                            features_products.setAdapter(fundupter);
-
-                            features_products.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                                @Override
-                                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-                                    category = vermiproHelperArrayListroHelper.get(position);
-
-                                    Intent intent = new Intent(FrontActivity.this, ProductDetailActivity.class);
-
-                                    Log.d("DATA",category.id);
-
-                                    intent.putExtra("product_id", category.id);
-                                    intent.putExtra("product_name", category.name);
-                                    intent.putExtra("product_price", category.price);
-                                    intent.putExtra("product_image", category.image);
-                                    intent.putExtra("product_unit", category.unit);
-                                    intent.putExtra("product_description", category.description);
-                                    startActivity(intent);
-
-                                }
-                            });
-
-                        }catch (Exception e){}
-
-                    }
-                });
-
-        postResponseAsyncTask.execute(URL+"category_products_limit_eight");
-        postResponseAsyncTask.setEachExceptionsHandler(new EachExceptionsHandler() {
-            @Override
-            public void handleIOException(IOException e) {
-                Toast.makeText(getApplicationContext(), "Internet connectivity is weak.", Toast.LENGTH_LONG).show();
-            }
-
-            @Override
-            public void handleMalformedURLException(MalformedURLException e) {
-                Toast.makeText(getApplicationContext(), "The URL is not well specified.", Toast.LENGTH_LONG).show();
-            }
-
-            @Override
-            public void handleProtocolException(ProtocolException e) {
-                Toast.makeText(getApplicationContext(), "Issue with protocol.", Toast.LENGTH_LONG).show();
-            }
-
-            @Override
-            public void handleUnsupportedEncodingException(UnsupportedEncodingException e) {
-                Toast.makeText(getApplicationContext(), "Text encoding is not proper.", Toast.LENGTH_LONG).show();
-            }
-        });
-
- }
-
-
-
-
-
-
-
-
+        }
 
 }
